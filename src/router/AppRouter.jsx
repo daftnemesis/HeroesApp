a49b2db6c792aa12b53ddd8fee2e8b7e095b53ce
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-06-23 23:03:07 -0500

    agregando estilos a la navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { DcPage } from '../heroes/pages/DcPage';
 import { MarvelPage } from '../heroes/pages/MarvelPage';
+import { Navbar } from '../ui/components/Navbar';
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path='marvel' element={ <MarvelPage /> } />
         <Route path='dc' element={ <DcPage /> } />
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,5 +1,49 @@
+import { NavLink, Link } from "react-router-dom"
+
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+
+    <nav className="bg-gradient-to-r from-violet-700 via-purple-700 to-fuchsia-700 border-gray-400 text-m font-medium p-2 shadow-lg">
+      <div className=" flex justify-between">
+        <div className="w-fit">
+          <Link
+            to='/'
+            className="text-white"
+          >
+            Asociaciones
+          </Link>
+        </div>
+
+        <div className="flex w-fit gap-3">
+          <div>
+            <NavLink 
+              to='marvel' 
+              className={ ({ isActive }) => `hover:text-white transition ease-in-out delay-100 ${isActive ? 'text-white': 'text-violet-500' }` }
+            >
+                Marvel
+            </NavLink>
+          </div>
+
+          <div>
+            <NavLink 
+              to='dc' 
+              className={ ({ isActive }) => `hover:text-white transition ease-in-out delay-100 ${isActive ? 'text-white': 'text-violet-500' }` }
+            >
+                DC
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="w-fit">
+          <div>
+            <button 
+              className="text-white"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
