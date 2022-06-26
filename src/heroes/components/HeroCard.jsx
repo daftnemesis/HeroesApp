122473d008507954c8af9c2f3fddcb021c191442
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-06-26 17:38:55 -0500

    dandole estilos a la page de heroes, y agregando boton de regreso

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 export const HeroCard = ({
   id,
   superhero,
@@ -8,18 +10,21 @@ export const HeroCard = ({
 }) => {
 
   const heroImgUrl = `/assets/heroes/${id}.jpg`;
-
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center bg-white rounded-lg border shadow-md hover:bg-gray-100">
+    <div className="flex flex-col md:flex-row items-center bg-white rounded-lg border shadow-md hover:bg-gray-100">
       <img src={heroImgUrl} alt={ superhero } className='object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg' />
-      <div class="flex flex-col justify-start p-4 leading-normal">
+      <div class="flex flex-col justify-start p-4 leading-normal h-auto">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{superhero}</h5>
         <p class="mb-3 font-normal text-gray-700">{alter_ego}</p>
         <p class="mb-3 font-normal text-gray-700">{first_appearance}</p>
+        <button 
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 focus:outline-none"
+          onClick={() => navigate(`/hero/${id}`)}
+        >
+          Conoce mas
+        </button>
       </div>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">
-        Conoce mas
-      </button>
     </div>
   )
 }
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,48 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const hero = getHeroById( id )
+
+  const onNavigateBack = () => {
+    navigate( -1 );
+  }
+
+  if (!hero) {
+    return <Navigate to='/marvel' />
+  }
+
   return (
-    <h1>Hero</h1>
+    <div className=" flex flex-col md:grid md:grid-cols-3 md:grid-rows-none gap-2">
+      <div>
+        <img 
+          src={`/assets/heroes/${ id }.jpg`}
+          alt={ hero.superhero }
+          className='rounded-xl'
+        />
+      </div>
+      <div className="col-start-2 col-end-4">
+        <h3 className="font-bold text-3xl">{ hero.superhero }</h3>
+        <ul className="mt-2">
+          <li className="font-medium text-base"> <b>Alter ego:</b> { hero.alter_ego} </li>
+          <li className="font-medium text-base"> <b>Publisher:</b> { hero.publisher} </li>
+          <li className="font-medium text-base"> <b>Primera aparicion:</b> {hero.first_appearance} </li>
+        </ul>
+        
+        <h5 className="font-bold text-xl mt-5">Characters</h5>
+        <p className="font-medium text-lg">{ hero.characters }</p>
+
+        <button
+          onClick={onNavigateBack}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+        >
+          Regresar
+        </button>
+      </div>
+    </div>
   )
 }
