536980270c6f5346b0c56d7f38282b7e3bfe3ff3
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-06-27 21:45:14 -0500

    usando useMemo para mejorar el rendimiendo de la app

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react";
 import { getHeroByPublisher } from "../helpers"
 import { HeroCard } from "./";
 
 export const HeroList = ({publisher}) => {
 
-  const heroes = getHeroByPublisher(publisher);
+  const heroes = useMemo( () => getHeroByPublisher( publisher ), [ publisher ] );
 
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
